bbcc7a195420d1df126f5295ae6bfa081dc621ba
山本尚幸 <naoyuki.yamamoto@persol.co.jp>
2017-11-10 19:02:17 +0900

    シャッフル＆タイマー機能追加

--- Puzzle15/ViewController.swift
+++ Puzzle15/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLable: UILabel!
+
     @IBOutlet weak var mainView: UIView!
     
     @IBOutlet weak var btn01: UIButton!
@@ -29,12 +31,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var btn15: UIButton!
     
     var numButtons: [UIButton] = []
+    var positions: [[CGFloat]] = []
     let btnWHSize: CGFloat = 90.0;
-
+    
+    weak var timer: Timer!
+    var startTime = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.timeLable.text = ""
         self.numButtons = getNumButtonArray()
+        self.positions = [
+            [0,0],          [btnWHSize,0],          [btnWHSize*2,0],            [btnWHSize*3,0],
+            [0,btnWHSize],  [btnWHSize,btnWHSize],  [btnWHSize*2,btnWHSize],    [btnWHSize*3,btnWHSize],
+            [0,btnWHSize*2],[btnWHSize,btnWHSize*2],[btnWHSize*2,btnWHSize*2],  [btnWHSize*3,btnWHSize*2],
+            [0,btnWHSize*3],[btnWHSize,btnWHSize*3],[btnWHSize*2,btnWHSize*3],  [btnWHSize*3,btnWHSize*3]
+        ]
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -229,5 +243,95 @@ class ViewController: UIViewController {
         return true
     }
     
+    func shuffleOne(){
+        var i = 0
+        for item in self.positions {
+            var flg = false
+            for _btn in numButtons {
+                if(item[0] == _btn.frame.origin.x && item[1] ==  _btn.frame.origin.y){
+                    flg = true
+                    break
+                }
+            }
+            if(flg == false){
+                break
+            }
+            i+=1
+        }
+        print(String(format: "%f, %f", self.positions[i][0], self.positions[i][1]))
+        let index = (Int)(arc4random_uniform(4))
+        var _x : CGFloat = 0
+        var _y : CGFloat = 0
+        switch index {
+        case 0:
+            _x = self.positions[i][0]
+            _y = self.positions[i][1] - btnWHSize
+        case 1:
+            _x = self.positions[i][0] - btnWHSize
+            _y = self.positions[i][1]
+        case 2:
+            _x = self.positions[i][0] + btnWHSize
+            _y = self.positions[i][1]
+        case 3:
+            _x = self.positions[i][0]
+            _y = self.positions[i][1] + btnWHSize
+        default:
+            _x = self.positions[i][0]
+            _y = self.positions[i][1]
+        }
+        
+        
+        for _btn in numButtons {
+            if (_btn.frame.origin.x == _x && _btn.frame.origin.y == _y){
+                _btn.frame = CGRect(x: self.positions[i][0], y: self.positions[i][1], width: btnWHSize, height: btnWHSize)
+                break
+            }
+        }
+    }
+    @IBAction func shuffle(_ sender: Any) {
+        var i = 0
+        while(i < 5000){
+            shuffleOne()
+            i += 1
+        }
+        startTimer()
+    }
+    
+    func startTimer() {
+        if timer != nil{
+            // timerが起動中なら一旦破棄する
+            timer.invalidate()
+        }
+        
+        timer = Timer.scheduledTimer(
+            timeInterval: 0.01,
+            target: self,
+            selector: #selector(self.timerCounter),
+            userInfo: nil,
+            repeats: true)
+        
+        startTime = Date()
+    }
+    @objc func timerCounter() {
+        // タイマー開始からのインターバル時間
+        let currentTime = Date().timeIntervalSince(startTime)
+        
+        // fmod() 余りを計算
+        let minute = (Int)(fmod((currentTime/60), 60))
+        // currentTime/60 の余り
+        let second = (Int)(fmod(currentTime, 60))
+        // floor 切り捨て、小数点以下を取り出して *100
+        let msec = (Int)((currentTime - floor(currentTime))*100)
+        
+        // %02d： ２桁表示、0で埋める
+//        let sMinute = String(format:"%02d", minute)
+//        let sSecond = String(format:"%02d", second)
+//        let sMsec = String(format:"%02d", msec)
+        
+        let time = String(format:"%02d:%02d.%02d", minute, second, msec)
+        print (time)
+        self.timeLable.text = time
+        
+    }
 }
 
